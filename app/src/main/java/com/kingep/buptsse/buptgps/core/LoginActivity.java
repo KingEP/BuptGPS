dfd6115fb1630fef2d6d5a36f73368eff432c61f
边茹月 <13051323471@163.com>
2017-10-31 12:59:42 +0800

    增加未完成的登录界面

---
+++ app/src/main/java/com/kingep/buptsse/buptgps/core/LoginActivity.java
@@ -0,0 +1,81 @@
+package com.kingep.buptsse.buptgps.core;
+
+import android.databinding.DataBindingUtil;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+
+import com.kingep.buptsse.buptgps.BaseActivity;
+import com.kingep.buptsse.buptgps.MainWindowActivity;
+import com.kingep.buptsse.buptgps.R;
+import com.kingep.buptsse.buptgps.databinding.ActivityLoginBinding;
+import com.kingep.buptsse.buptgps.http.BaseOkHttpCallBack;
+import com.kingep.buptsse.buptgps.http.LoginManagerClient;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+
+/**
+ * Created by Dell on 2017/10/30.
+ */
+
+public class LoginActivity extends BaseActivity {
+
+    private ActivityLoginBinding mBinding;
+
+    @Override
+    protected void getLayoutResource() {
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+    }
+
+    @Override
+    protected void initView() {
+        mBinding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = mBinding.usernameText.getText().toString();
+                String password = mBinding.passwordText.getText().toString();
+
+           /*     if(TextUtils.isEmpty(username)){
+                    mBinding.usernameText.setError("用户名不能为空！");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    mBinding.passwordText.setError("密码不能为空！");
+                    return;
+                }
+                LoginManagerClient.login_get(username, password, new BaseOkHttpCallBack() {
+                    @Override
+                    public void OnSuccess(String content, int result, String resultDescription) {
+                        Toast.makeText(LoginActivity.this,"登录成功",Toast.LENGTH_SHORT).show();
+                        JumpToActivity(MainWindowActivity.class);
+                        LoginActivity.this.finish();
+                    }
+
+                    @Override
+                    public void OnError(String desc, int result) {
+                        Toast.makeText(LoginActivity.this,"登录失败，"+desc,Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void OnFailure(Call call, IOException e) {
+
+                    }
+                });*/
+
+
+                JumpToActivity(MainWindowActivity.class);
+                LoginActivity.this.finish();
+
+            }
+        });
+        mBinding.registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/kingep/buptsse/buptgps/http/LoginManagerClient.java
@@ -0,0 +1,35 @@
+package com.kingep.buptsse.buptgps.http;
+
+import okhttp3.Call;
+import okhttp3.FormBody;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+/**
+ * Created by Yu on 2017/5/24.
+ */
+
+public class LoginManagerClient {
+    private static BaseOkHttpClient client = BaseOkHttpClient.getInstance();
+    /**
+     * okhttp登陆 get请求方式
+     * @return
+     */
+    public static Call login_get(String username,String password,BaseOkHttpCallBack baseOkHttpCallBack){
+        Request request = new Request.Builder().url(client.getAbsoluteUrl("loginServlet?username="+username+"&password="+password)).build();
+       // Request request = new Request.Builder().url("http://www.baidu.com").build();
+        return callOkHttpRequest(request,baseOkHttpCallBack);
+    }
+    /**
+     * okhttp登陆 Post请求方式
+     * @return
+     */
+    public static Call login_post(String username,String password,BaseOkHttpCallBack baseOkHttpCallBack){
+        RequestBody body = new FormBody.Builder().add("username",username).add("password",password).build();
+        Request request = new Request.Builder().post(body).url(client.getAbsoluteUrl("loginServlet")).build();
+        return callOkHttpRequest(request,baseOkHttpCallBack);
+    }
+    protected static Call callOkHttpRequest(Request request, BaseOkHttpCallBack baseOkHttpCallBack){
+        return client.callOkHttpRequest(request,baseOkHttpCallBack);
+    }
+}
